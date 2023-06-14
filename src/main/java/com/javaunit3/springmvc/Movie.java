b74736bec9833fd77977772ee00e6f0485dbd892
DJRoche509 <davjroche@gmail.com>
2023-06-14 17:11:01 -0500

    Initial Commit of interface Movie.java

---
+++ src/main/java/com/javaunit3/springmvc/Movie.java
@@ -0,0 +1,9 @@
+package com.javaunit3.springmvc;
+
+// Interface
+interface Movie{
+    // Interface methods
+    public String getTitle();
+    public String getMaturityRating();
+    public String getGenre();
+}
