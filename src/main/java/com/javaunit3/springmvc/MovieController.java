7fd7b6ecd88f36353c02a6c04057c51f98c46b93
DJRoche509 <davjroche@gmail.com>
2023-06-16 00:49:31 -0500

    Define class MovieController & annotate it w/ SpringBootApplication + RequestMapping

--- src/main/java/com/javaunit3/springmvc/MovieController.java
+++ src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -1,4 +1,12 @@
 package com.javaunit3.springmvc;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
 public class MovieController {
+    @RequestMapping("/")
+    public String getIndexPage(){
+        return "index";
+    }
 }
