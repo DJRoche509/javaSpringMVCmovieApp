4210ef3618ef30f8e87b08a7eb00da2cc2c93b76
DJRoche509 <davjroche@gmail.com>
2023-06-16 00:47:36 -0500

    Add an import statement in SpringBootApplication.java

--- src/main/java/com/javaunit3/springmvc/SpringmvcApplication.java
+++ src/main/java/com/javaunit3/springmvc/SpringmvcApplication.java
@@ -1,10 +1,11 @@
 package com.javaunit3.springmvc;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringmvcApplication{
     public static void main(String[] args) {
-        SpringmvcApplication.run(SpringmvcApplication.class, args);
+        SpringApplication.run(SpringmvcApplication.class, args);
     }
 }
