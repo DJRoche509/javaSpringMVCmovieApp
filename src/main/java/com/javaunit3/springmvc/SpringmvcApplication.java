58622651e63fadb82d8f46e74545df4b6d4cec6b
DJRoche509 <davjroche@gmail.com>
2023-06-16 00:32:52 -0500

    Annotate file with SpringBootApplication and .run() w/ 2 params

---
+++ src/main/java/com/javaunit3/springmvc/SpringmvcApplication.java
@@ -0,0 +1,10 @@
+package com.javaunit3.springmvc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcApplication{
+    public static void main(String[] args) {
+        SpringmvcApplication.run(SpringmvcApplication.class, args);
+    }
+}
