182ca936fed18edcbb8e7ae2088e087686435b6e
DJRoche509 <davjroche@gmail.com>
2023-06-16 02:07:45 -0500

    Add model.addAttribute

--- src/main/java/com/javaunit3/springmvc/MovieController.java
+++ src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -1,6 +1,7 @@
 package com.javaunit3.springmvc;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
@@ -8,5 +9,12 @@ public class MovieController {
     @RequestMapping("/")
     public String getIndexPage(){
         return "index";
+    }
+
+    @RequestMapping("/")
+    public String getBestMoviePage(Model model){
+        BestMovieService bestMovieService = null;
+        model.addAttribute("BestMovie", bestMovieService.getBestMovie().getTitle());
+        return ("bestMovie");
     }
 }
