f06b0c2abd6fefa8714f6f3164b66f4c46c7f84f
DJRoche509 <davjroche@gmail.com>
2023-06-18 03:55:27 -0500

    Update with addAnnotatedClass()

--- src/main/java/com/javaunit3/springmvc/HibernateConfig.java
+++ src/main/java/com/javaunit3/springmvc/HibernateConfig.java
@@ -13,6 +13,7 @@ public class HibernateConfig {
         SessionFactory factory = new org.hibernate.cfg.Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(MovieEntity.class)
+                .addAnnotatedClass(VoteEntity.class)
                 .buildSessionFactory();
 
         return factory;
