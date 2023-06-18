728fd0c0e660dd56c1513357a9db8b88f342ee7e
DJRoche509 <davjroche@gmail.com>
2023-06-18 03:17:00 -0500

    Added Form list and other attributes

--- src/main/java/com/javaunit3/springmvc/MovieController.java
+++ src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -1,5 +1,7 @@
 package com.javaunit3.springmvc;
 
+import com.javaunit3.springmvc.model.MovieEntity;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class MovieController {
@@ -31,6 +34,15 @@ public class MovieController {
 
     @RequestMapping("/voteForBestMovieForm")
     public String voteForBestMovieFormPage(Model model) {
+        Session session = sessionFactory.getCurrentSession();
+
+        session.beginTransaction();
+
+        List<MovieEntity> movieEntityList = session.createQuery("from MovieEntity").list();
+
+        session.getTransaction().commit();
+
+        model.addAttribute("movies", movieEntityList);
         return "voteForBestMovie";
     }
 
@@ -42,5 +54,34 @@ public class MovieController {
         model.addAttribute("BestMovieVote", movieTitle);
 
         return "voteForBestMovie";
+    }
+
+    @RequestMapping("/addMovieForm")
+    public String addMovieForm()
+    {
+        return "addMovie";
+    }
+
+    @RequestMapping("/addMovie")
+    public String addMovie(HttpServletRequest request)
+    {
+        String movieTitle = request.getParameter("movieTitle");
+        String maturityRating = request.getParameter("maturityRating");
+        String genre = request.getParameter("genre");
+
+        MovieEntity movieEntity = new MovieEntity();
+        movieEntity.setTitle(movieTitle);
+        movieEntity.setMaturityRating(maturityRating);
+        movieEntity.setGenre(genre);
+
+        Session session = sessionFactory.getCurrentSession();
+
+        session.beginTransaction();
+
+        session.save(movieEntity);
+
+        session.getTransaction().commit();
+
+        return "addMovie";
     }
 }
