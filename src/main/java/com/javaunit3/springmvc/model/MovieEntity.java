73c63d0ca2f41d64c0ad3c91e926b602e000d900
DJRoche509 <davjroche@gmail.com>
2023-06-17 22:20:15 -0500

    Added getters & setters to private properties

--- src/main/java/com/javaunit3/springmvc/model/MovieEntity.java
+++ src/main/java/com/javaunit3/springmvc/model/MovieEntity.java
@@ -8,7 +8,7 @@ public class MovieEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "movie_Id")
-    private int id;
+    private Integer id;
 
     @Column(name = "title")
     private String title;
@@ -18,4 +18,36 @@ public class MovieEntity {
 
     @Column(name = "genre")
     private String genre;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMaturityRating() {
+        return maturityRating;
+    }
+
+    public void setMaturityRating(String maturityRating) {
+        this.maturityRating = maturityRating;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
